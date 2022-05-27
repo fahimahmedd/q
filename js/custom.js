48c12dc179e45491fb0295a3543cef918d9da65d
fahimahmedd <105433729+fahimahmedd@users.noreply.github.com>
2022-05-27 13:59:57 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+$(function(){
+
+// Marque Part  
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+     $('.header_marque').addClass('marqueNewClass');
+  } else {
+     $('.header_marque').removeClass('marqueNewClass');
+  }
+});
+
+// Navbar Part  
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 10) {
+     $('.navBar').addClass('navBarNewClass');
+  } else {
+     $('.navBar').removeClass('navBarNewClass');
+  }
+});
+// Mobile Navbar Part  
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 10) {
+     $('.mob_header').addClass('mobNewHeader');
+  } else {
+     $('.mob_header').removeClass('mobNewHeader');
+  }
+});
+// guest Slider
+// $('.CampainBanner_wrapper').slick({
+//   infinite: true,
+//   slidesToShow: 1,
+//   slidesToScroll:1,
+//   arrows:true,
+//   autoplay: true, 
+//   autoplaySpeed: 4000,
+//   speed: 800,
+//   dots:false,
+//   prevArrow:'<i class="fas fa-chevron-left cardPrev"></i>', 
+//   nextArrow:'<i class="fas fa-chevron-right cardNext"></i>',
+//   responsive: [
+//     {
+//       breakpoint: 968,
+//       settings: {
+//         arrows: true,
+//         centerMode: true,
+//         centerPadding: '0px',
+//         slidesToShow: 1
+//       }
+//     },
+//     {
+//       breakpoint: 767,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '0px',
+//         slidesToShow: 1
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '0px',
+//         slidesToShow:1
+//       }
+//     }
+//   ]
+// });
+
+   // Back to Top 
+   var btn = $('.backtotop');
+   $(window).scroll(function() {
+     if ($(window).scrollTop() > 300) {
+       btn.addClass('show');
+     } else {
+       btn.removeClass('show');
+     }
+   });
+   btn.on('click', function(e) {
+     e.preventDefault();
+     $('html, body').animate({scrollTop:0}, '300');
+   });
+ 
+   
+
+
+
+});
+
